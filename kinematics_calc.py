9024d36a31b1410e799171e8b1ce0ed8d0318683
JoshuaT35 <joshuatablan03@gmail.com>
2025-03-27 11:16:43 -0600

    reorganization

--- kinematics_calc.py
+++ kinematics_calc.py
@@ -1,3 +1,6 @@
+# NOTE
+# instead of list, use custom struct/class
+
 '''
 Given:
 - 3 values [ax, ay, az] denoting the current reading in (m/s^2)
@@ -6,20 +9,31 @@ Given:
 
 Return:
     - the displacement as [sx, sy, sz] in (m)
-    - the final (current) velocity as [vx, vy, vz] in (m)
 '''
-def get_displacement_and_final_vel(current_accel, prev_vel, delta_time):
+def get_displacement(current_accel, prev_vel, delta_time):
     # using s = ut + 1/2(a)(t^2)
     displacement_x = prev_vel[0]*delta_time + 0.5*(current_accel[0])*(delta_time**2)
     displacement_y = prev_vel[1]*delta_time + 0.5*(current_accel[1])*(delta_time**2)
     displacement_z = prev_vel[2]*delta_time + 0.5*(current_accel[2])*(delta_time**2)
 
-    # using v = u + at
-    final_vel_x = prev_vel[0] + current_accel[0]*delta_time
-    final_vel_y = prev_vel[1] + current_accel[1]*delta_time
-    final_vel_z = prev_vel[2] + current_accel[2]*delta_time
+    return list(displacement_x, displacement_y, displacement_z)
 
-    return list(displacement_x, displacement_y, displacement_z), list(final_vel_x, final_vel_y, final_vel_z)
+'''
+Given:
+- 3 values [ax, ay, az] denoting the current reading in (m/s^2)
+- velocity of object at previous position [ux, uy, uz] in (m/s)
+- The time between the previous and current reading in (s)
+
+Return:
+    - the final (current) velocity as [vx, vy, vz] in (m)
+'''
+def get_current_vel(current_accel, prev_vel, delta_time):
+    # using v = u + at
+    current_vel_x = prev_vel[0] + current_accel[0]*delta_time
+    current_vel_y = prev_vel[1] + current_accel[1]*delta_time
+    current_vel_z = prev_vel[2] + current_accel[2]*delta_time
+
+    return list(current_vel_x, current_vel_y, current_vel_z)
 
 '''
 Given:
@@ -30,15 +44,14 @@ Given:
 
 Return:
     - the current position [d2x, d2y, d2z]
-    - the current velocity [vx, vy, vz]
 '''
 def get_current_position(current_accel, prev_vel, prev_position, delta_time):
-    # get the displacement and final velocity
-    displacement, final_velocity = get_displacement_and_final_vel(current_accel, prev_vel, delta_time)
+    # get the displacement
+    displacement = get_displacement(current_accel, prev_vel, delta_time)
 
-    # get position
+    # get position (previous position + change in distance)
     current_pos_x = prev_position[0] + displacement[0]
     current_pos_y = prev_position[1] + displacement[1]
     current_pos_z = prev_position[2] + displacement[2]
 
-    return list(current_pos_x, current_pos_y, current_pos_z), final_velocity
+    return list(current_pos_x, current_pos_y, current_pos_z)
--- main.py
+++ main.py
@@ -1,122 +1,27 @@
 import asyncio
-from bluetooth import ble_connect_imu
-from visualization import start_plot
-from kinematics_calc import get_current_position
 import matplotlib.pyplot as plt
 
-## --- 2d plotting ---
-async def print_data(data_queue):
-    while True:
-        data = await data_queue.get()
-        print(f"Received data")
-        print("\n")
-        t, x, y, z = data
-        
-        # Append the data to respective lists for plotting
-        timestamp.append(t)
-        xdata.append(x)
-        ydata.append(y)
-        zdata.append(z)
-        
-        # Clear the plot and redraw it with the updated data
-        ax.clear()
-        
-        # Plot x, y, z values with respect to time (timestamp)
-        ax.plot(timestamp, xdata, label="X", color="r")
-        ax.plot(timestamp, ydata, label="Y", color="g")
-        ax.plot(timestamp, zdata, label="Z", color="b")
-        
-        ax.set_xlabel('Time (ms)')  # Label for x-axis
-        ax.set_ylabel('Values')  # Label for y-axis
-        ax.set_title('Real-Time Plot of x, y, z')  # Title of the plot
-        
-        ax.legend()  # Show legend
-        
-        plt.draw()  # Redraw the plot
-        plt.pause(0.05)  # Non-blocking pause to update the plot
+from bluetooth import ble_connect_imu
+from visualization import plot_2d_data
 
 async def main():
+    # figure and axis for 2D plot
+    fig, ax = plt.subplots()
+
+    # stores collected data
     data_queue = asyncio.Queue()
 
-    # Start BLE connection and data collection
+    # create BLE connection and data collection tasks
     ble_task = asyncio.create_task(ble_connect_imu(data_queue))
-    print_task = asyncio.create_task(print_data(data_queue))
+    plot_task = asyncio.create_task(plot_2d_data(ax, data_queue))
 
-    await ble_task  # Keeps the BLE connection alive
-    await print_task
+    # main waits for these tasks to complete (which is ideally never)
+    await ble_task
+    await plot_task
 
-# Setup the 2D plot
-fig, ax = plt.subplots()  # Create a figure and axis for 2D plot
-timestamp, xdata, ydata, zdata = [], [], [], []  # Data lists to store timestamps and x, y, z values
-asyncio.run(main())
-plt.show()
+    # idk if this works, might be a replacement for the above awaits
+    # await asyncio.gather(ble_task, plot_task)
 
-## --- 3d plotting ---
-# async def plot_data(data_queue):
-#     initial_reading = True
-#     while True:
-#         # get data
-#         data = await data_queue.get()
-#         timestamp, ax, ay, az = data
-
-#         # if initial reading, wait until we get the next reading
-#         if initial_reading:
-#             initial_reading = False
-#             prev_time = timestamp
-#             prev_vel = [0, 0, 0]
-#             prev_pos = [0, 0, 0]
-#         # secondary+ data points obtained
-#         else:
-#             print("here now\n")
-#             # get change in time and update prev time
-#             delta_time = timestamp - prev_time
-#             prev_time = timestamp
-#             print("1\n")
-
-#             # get the current position and velocity
-#             prev_pos, prev_vel = get_current_position([ax, ay, az], prev_vel, prev_pos, delta_time)
-#             print("2\n")
-
-#             # append new data
-#             xdata.append(prev_pos[0])
-#             ydata.append(prev_pos[1])
-#             zdata.append(prev_pos[2])
-#             print("over here\n")
-
-#         # xdata.append(ax)
-#         # ydata.append(ay)
-#         # zdata.append(az)
-#         print("hit!")
-#         axes.clear()  # Clear previous data
-#         axes.plot3D(xdata, ydata, zdata, 'gray')  # Plot the new data
-#         plt.draw()  # Redraw the plot
-#         plt.pause(0.05)  # Non-blocking pause to update the plot
-
-
-# async def main():
-#     data_queue = asyncio.Queue()
-
-#     # Start BLE connection and data collection
-#     ble_task = asyncio.create_task(ble_connect_imu(data_queue))
-#     plot_task = asyncio.create_task(plot_data(data_queue))
-
-#     await ble_task  # Keeps the BLE connection alive
-#     await plot_task
-
-# # Setup the 3D plot
-# fig = plt.figure()
-# axes = fig.add_subplot(111, projection='3d')
-# xdata = []
-# ydata = []
-# zdata = []
-
-# # calulation data
-# prev_vel = [0, 0, 0]
-# prev_pos = [0, 0, 0]
-# prev_time = 0
-
-# # run async tasks
-# asyncio.run(main())
-
-# # show the plot
-# plt.show()
+if __name__ == "__main__":
+    asyncio.run(main())
+    plt.show()
